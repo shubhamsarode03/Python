58d2c3b2862562933df5b7650f21ae672b26d95f
Shubham Sarode <sarodeshubham02@gmail.com>
2025-03-07 23:26:59 +0530

    dict

---
+++ PyNative/Dictionary/change value in nested dict .py
@@ -0,0 +1,23 @@
+# Exercise 10: Change value of a key in a nested dictionary
+# Write a Python program to change Brad’s salary to 8500 in the following dictionary.
+
+# Given:
+
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+# Expected output:
+
+# {
+#    'emp1': {'name': 'Jhon', 'salary': 7500},
+#    'emp2': {'name': 'Emma', 'salary': 8000},
+#    'emp3': {'name': 'Brad', 'salary': 8500}
+# }
+
+def change_value(sample_dict):
+    sample_dict["emp3"]["salary"]= 8500
+    return sample_dict
+
+print(change_value(sample_dict))
